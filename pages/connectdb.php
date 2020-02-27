fc7c86b3ac9a91458fc64537f271b7d2a3c0c447
Erick Trigueros <59934930+ErickTrigueros@users.noreply.github.com>
2020-02-27 10:40:17 -0600

    Revert "Se agrego cambio de imagen de usuario, y vista del detalle de producto desde el modulo user"

--- pages/connectdb.php
+++ pages/connectdb.php
@@ -2,8 +2,8 @@
 
     try {
          //Don't write blank spaces in the databae connection
-        $pdo = new PDO('mysql:host=localhost;dbname=u422793738_panamerican','u422793738_panam','Panam2020');//Production
-        //$pdo = new PDO('mysql:host=localhost;dbname=panamericanstore_db','root','');//Develop
+        //$pdo = new PDO('mysql:host=localhost;dbname=u422793738_panamerican','u422793738_panam','Panam2020');//Production
+        $pdo = new PDO('mysql:host=localhost;dbname=panamericanstore_db','root','');//Develop
         //echo 'Connection Succesfull';
     } catch (PDOException $f) {
         echo $f->getmessage();
